eab804d0854071c09b9d695f70bdbf85600764c3
andrejanchevski <andrej.anchevski@sorsix.com>
2020-09-14 01:42:35 +0200

    cart-fragment changed

---
+++ app/src/main/java/com/buymee/cart/CartFragment.kt
@@ -0,0 +1,37 @@
+package com.buymee.cart
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.buymee.R
+import kotlinx.android.synthetic.main.fragment_cart.*
+
+class CartFragment : Fragment() {
+
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_cart, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewManager = LinearLayoutManager(activity)
+        viewAdapter = CartAdapter()
+        cart_recycler_view.apply {
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+    }
+
+
+}
